c755cf5e3974c23ff226279a6491c31299085aed
Alan Gordon <Alan.M.Gordon@gmail.com>
2012-09-10 22:46:39 -0700

    all operations are now on pth object

--- MultiLevelPathAnalysis2.R
+++ MultiLevelPathAnalysis2.R
@@ -1,120 +1,127 @@
+makePathObj <- function(paths, DF){
+  pth = list()
+  pth$paths = paths
+  pth$DF = DF
+  pth$varNames = attributes(DF)$names
+  pth$nVars = length(pth$varNames)
+  pth$connectionMatrix = matrix()
+  pth$coefMatrix = matrix()
+  pth$indirectPaths = list()
+  pth$indirectPathCoefs = list()
+  return (pth)
+}
+
 #create matrix of direct connections
-makeConnectionMatrix <- function(paths, DF){
+makeConnectionMatrix <- function(pth){
   
-  varNames = attributes(DF)$names
-  nVars = length(varNames)
-  connectionMatrix = matrix(0, nVars, nVars)
-  rownames(connectionMatrix) = varNames
-  colnames(connectionMatrix) = varNames
+  connectionMatrix = matrix(0, pth$nVars, pth$nVars)
+  rownames(connectionMatrix) = pth$varNames
+  colnames(connectionMatrix) = pth$varNames
   
-  for( i in 1:length(paths) ){
-    varNames = attributes(DF)$names
-    thisPath<-paths[i]
+  for( i in 1:length(pth$paths) ){
+    thisPath<-pth$paths[i]
     theseVars<-strsplit(thisPath, "->")[[1]]
-    V1<-which(theseVars[1] == varNames)
-    V2<-which(theseVars[2] == varNames)
+    V1<-which(theseVars[1] == pth$varNames)
+    V2<-which(theseVars[2] == pth$varNames)
     connectionMatrix[V1,V2] = 1
   }
-  return(connectionMatrix)
+  pth$connectionMatrix = connectionMatrix
+  return(pth)
 }
 
 # create matrix of direct path coefficients	
-estDirectCoefficients <- function(connectionMatrix, DF){
-  coefMatrix = connectionMatrix
+estDirectCoefficients <- function(pth){
+  coefMatrix = pth$connectionMatrix
   coefMatrix[,] = 0
-  varNames = colnames(connectionMatrix)
-  for( i in 1:ncol(connectionMatrix) ){
-    
-    thisCol<-connectionMatrix[,i]
+  for( i in 1:ncol(pth$connectionMatrix) ){    
+    thisCol<-pth$connectionMatrix[,i]
     
     if(sum(thisCol)>0){
-      IVs<-varNames[thisCol==1]
-      DV = varNames[i]
-      
+      IVs<-pth$varNames[thisCol==1]
+      DV = pth$varNames[i]      
       IVsCombined	<- paste( c(IVs) , collapse = "+" )
       thisFormula <- as.formula( paste( DV, IVsCombined , sep = "~" ) )
-      theseCoef <- coefficients( summary( lm(thisFormula, DF ) ) )[,"Estimate"]
+      theseCoef <- coefficients( summary( lm(thisFormula, pth$DF ) ) )[,"Estimate"]
       
       coefMatrix[which(thisCol==1),i] = theseCoef[2:length(theseCoef)]
     }
   }
-  return(coefMatrix)
+  pth$coefMatrix = coefMatrix
+  return(pth)
 }
 
+findIndirectPathsWrapper <- function(pth) {
+  pth$ix<-0
+  pth = findIndirectPaths(pth, pth$varNames)
+  pth$thisIP<-NULL
+  pth$ix<-NULL
+  
+  #trim off repeat paths (unclear if this is still needed)
+  pth$indirectPaths<-unique(pth$indirectPaths)
+  
+  return(pth)
+}
 
 # recursively find all the indirect paths
-findIndirectPaths <- function(coefMatrix, IPs, varsToSearch){
-  
-  allVars<-rownames(coefMatrix)
+findIndirectPaths <- function(pth, varsToSearch){  
   
   for(v in varsToSearch){	
     
-    i = which(allVars==v)
-    thisRow<-coefMatrix[i,]
-    thisVar<-rownames(coefMatrix)[i]
-    IPs$thisIP = c(IPs$thisIP, thisVar) 	
+    i = which(pth$varNames==v)
+    thisRow<-pth$coefMatrix[i,]
+    thisVar<-pth$varNames[i]
+    pth$thisIP = c(pth$thisIP, thisVar) 	
     
-    if (length(IPs$thisIP) > 2){
-      
-      IPs$ix <- IPs$ix + 1 # update path index
-      IPs$IPlist[[IPs$ix]]<-IPs$thisIP # include this path in the list 
+    if (length(pth$thisIP) > 2){      
+      pth$ix <- pth$ix + 1 # update path index
+      pth$indirectPaths[[pth$ix]]<-pth$thisIP # include this path in the list 
+      print(pth$ix)
     }
     
     if(sum(abs(thisRow))>0){
-      newVarsToSearch = allVars[which(thisRow!=0)]
+      newVarsToSearch = pth$varNames[which(thisRow!=0)]
       newCoefMatrix = coefMatrix
       newCoefMatrix[i,] = 0
-      IPs = findIndirectPaths(coefMatrix, IPs, newVarsToSearch)
+      pth = findIndirectPaths(pth, newVarsToSearch)
     }
-    IPs$thisIP = IPs$thisIP[-length(IPs$thisIP)]
+    pth$thisIP = pth$thisIP[-length(pth$thisIP)]
   }
-  return(IPs)
+  return(pth)
 }
 
 
 # compute all indirect path coefficients
-indirectPathCoefs <- function(coefMatrix, allIndirectPathDes){
-  varNames = colnames(coefMatrix)
+indirectPathCoefs <- function(pth){
+
   allIndirectPathCoef = list()
-  for( i in 1:length(allIndirectPathDes) ){
-    thisPath = allIndirectPathDes[[i]]
+  for( i in 1:length(pth$indirectPaths) ){
+    thisPath = pth$indirectPaths[[i]]
     thisIndCoef = 1
     for( j in 1:(length(thisPath)-1) ){
       
-      V1<-which(thisPath[j] == varNames)
-      V2<-which(thisPath[j+1] == varNames)
+      V1<-which(thisPath[j] == pth$varNames)
+      V2<-which(thisPath[j+1] == pth$varNames)
       
-      thisDirCoef = coefMatrix[V1,V2];
+      thisDirCoef = pth$coefMatrix[V1,V2];
       thisIndCoef = thisIndCoef * thisDirCoef      
     }
     allIndirectPathCoef[[i]] = thisIndCoef
   }
-  return(allIndirectPathCoef)
+  pth$indirectPathCoefs = allIndirectPathCoef
+  return(pth)
 }	
 
 # The main function.  Given some paths and a data frame, run a path analysis
 pathAnalysis <- function(paths, DF)	{
   
-  conMatrix = makeConnectionMatrix(paths,DF)
-  coefMatrix = estDirectCoefficients(conMatrix,DF)
+  pth = makePathObj(paths, DF)
   
-  IPs = list()
-  IPs$ix = 0
-  IPs$IPlist = list()
-  IPs$thisIP = NULL
+  pth = makeConnectionMatrix(pth)
+  pth = estDirectCoefficients(pth)
+  pth = findIndirectPathsWrapper(pth)  
+  pth = indirectPathCoefs(pth)
   
-  IPs<-findIndirectPaths(coefMatrix, IPs, attributes(DF)$names)
-  
-  #trim off repeat paths (though I don't think these should exist)
-  allIndirectPathDes<-unique(IPs$IPlist)
-  
-  indCoefs<-indirectPathCoefs(coefMatrix, allIndirectPathDes)
-  
-  res = list()
-  res$coefMatrix = coefMatrix
-  res$allIndirectPathDes = allIndirectPathDes
-  res$indCoefs = indCoefs
-  return(res)
+  return(pth)
 }
 
 #Usage Case
@@ -131,7 +138,7 @@ paths = c('IV->MV1', 'IV->MV2', 'MV1->DV', 'MV2->DV')
 
 pathRes<-pathAnalysis(paths, DF)
 
-##TO DO: pass around an object with things like varnams and nVars already included in it
+##TO DO: 
 # better connect the ind coefficients and their descriptions
 # check for crazy input, non-semantic input, and cyclic connections (which would invalidate this procedure)
 #implement bootstrapping for significance testing of ind coefficients
